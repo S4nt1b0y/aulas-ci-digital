c9e9a93f097fd22083f08cd3386cf8e5ba51d138
S4nt1b0y <gustavo.santiago@cepedi.org.br>
2026-07-25 15:53:03 -0300

    report da ultima sintese

--- aula_25_07/LAB02/E6/synth_flow.cmd
+++ aula_25_07/LAB02/E6/synth_flow.cmd
@@ -1,8 +1,7 @@
 # Cadence Genus(TM) Synthesis Solution, Version 25.12-s067_1, built Nov 17 2025 13:10:10
 
-# Date: Sat Jul 25 15:44:12 2026
+# Date: Sat Jul 25 15:49:30 2026
 # Host: localhost.localdomain (x86_64 w/Linux 5.14.0-687.29.1.el9_8.x86_64) (8cores*16cpus*1physical cpu*AMD Ryzen 7 5700G with Radeon Graphics 512KB)
 # OS:   Rocky Linux 9.8 (Blue Onyx)
 
 source ./script.tcl
-exit
